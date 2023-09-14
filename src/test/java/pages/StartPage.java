5dc807dcff9887f2e2f6d64a8d12bf9c9df14624
Bence <banikbence12@gmail.com>
2023-09-14 11:51:00 +0200

    feature a working list own journals feature

---
+++ src/test/java/pages/StartPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class StartPage extends BasePage {
+    By signInButton = By.xpath("//*[@class='MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium css-rpnln0']");
+
+    public void clickSignInButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(signInButton));
+        driver.findElement(signInButton).click();
+    }
+}
---
+++ src/test/java/steps/LIstOwnJournalSteps.java
@@ -0,0 +1,47 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.jupiter.api.Assertions;
+import pages.BasePage;
+import pages.LoginPage;
+import pages.MainPage;
+import pages.StartPage;
+import util.WebDriverManager;
+
+public class LIstOwnJournalSteps {
+
+    private LoginPage loginPage;
+    private MainPage mainPage;
+    private StartPage startPage;
+
+    public LIstOwnJournalSteps() {
+        this.loginPage = new LoginPage();
+        this.mainPage = new MainPage();
+        this.startPage = new StartPage();
+    }
+
+    @Given("I log in to the site as {string}")
+    public void iLogInToTheSiteAs(String url) {
+        loginPage.navigateToSite(url);
+
+    }
+
+    @And("I fill the required data as {string} and {string}")
+    public void iFillTheRequiredDataAsAnd(String userEmail, String userPassword) {
+        startPage.clickSignInButton();
+        loginPage.enterUserEmail(userEmail);
+        loginPage.enterPassword(userPassword);
+        loginPage.clickLoginBtn();
+    }
+
+    @Then("I should see the journal as {string}")
+    public void iShouldSeeTheJournalAs(String journals) {
+        Assertions.assertTrue(mainPage.isJournalPresent());
+        WebDriverManager.quitDriver();
+    }
+
+
+}
+
